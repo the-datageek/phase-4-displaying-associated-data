3cf9881abe314f42180ab620b2d6a5144b54c3b7
the-datageek <moraaf770@gmail.com>
2023-03-31 09:51:37 +0300

    Done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
+
 class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
 
 end
